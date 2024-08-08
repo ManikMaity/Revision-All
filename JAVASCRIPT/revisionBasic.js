5ebf9aef2755f7e7282477bb4896a089820c6bb8
Manik Maity <manikmaity156@gmail.com>
2024-08-08 22:25:16 +0530

    Revision upto ex day 5 - arrays

--- JAVASCRIPT/revisionBasic.js
+++ JAVASCRIPT/revisionBasic.js
@@ -7,9 +7,8 @@
 // })
 
 const print = (data) => {
-    console.log(data);
-}
-
+  console.log(data);
+};
 
 // Day 01
 
@@ -18,13 +17,11 @@ const print = (data) => {
 /* comments can make code readable, 
 easy to reuse and informative */
 
-
 // const num = 12;
 // const name = "Manik";
 // const isTrue = false;
 // const what = null;
 // let title;
-
 
 // console.log(typeof num);
 // console.log(typeof name);
@@ -38,8 +35,7 @@ easy to reuse and informative */
 // object
 // undefined
 
-
-// storing variables in single line 
+// storing variables in single line
 // const fName = "Manik", lNmae = "Maity", age = 21, isMarried = false, country = "India";
 // print([fName, lNmae, age, isMarried, country]);
 // // [ 'Manik', 'Maity', 21, false, 'India' ]
@@ -51,32 +47,29 @@ My age is 21.
 Your age is 20.
 */
 
-
-
-
 // DAY 02 EXERCISE
 console.log("Manik".length); // 5
 
 const stringMethod = (str = "") => {
-    str = str.trim();
-    console.log(str.toUpperCase());
-    print(str.toLocaleLowerCase());
-    print(str.substring(0, 5));
-    print(str.substr(0, 4));
-    console.log(str.toLocaleLowerCase().includes("script")); 
-    print(str.replace("JavaScript", "Phython"));  
-    print(str.charAt(2));;
-    print(str.toLocaleLowerCase().indexOf("a"));
-    print(str.lastIndexOf("a"))
-
-}
+  str = str.trim();
+  console.log(str.toUpperCase());
+  print(str.toLocaleLowerCase());
+  print(str.substring(0, 5));
+  print(str.substr(0, 4));
+  console.log(str.toLocaleLowerCase().includes("script"));
+  print(str.replace("JavaScript", "Phython"));
+  print(str.charAt(2));
+  print(str.toLocaleLowerCase().indexOf("a"));
+  print(str.lastIndexOf("a"));
+};
 
 // stringMethod("Manik is a good boy");\
 
-const str1 = "I love JavaScript. If you do not love JavaScript what else can you love.";
+const str1 =
+  "I love JavaScript. If you do not love JavaScript what else can you love.";
 console.log(str1.match(/love/gi));
 
-const company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' ;
+const company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
 // print(company.split(", "));
 
 // console.log("30 Days Of JavaScript".split(" ").filter((word) => word !== "30").join(" "));
@@ -90,8 +83,8 @@ const company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' ;
 
 // generateRandomNum(5);
 
-for (let i = 1; i <= 5; i++){
-    console.log(`${i} ${i} ${i*i} ${i**3}`);
+for (let i = 1; i <= 5; i++) {
+  console.log(`${i} ${i} ${i * i} ${i ** 3}`);
 }
 
 /*
@@ -110,14 +103,11 @@ for (let i = 1; i <= 5; i++){
 5 1 5 25 125
 */
 
-
-
 // const countWordInSentance = (sentance = "") => {
 //     console.log(sentance.match(/love/gi).length);
 // }
 
 // // countWordInSentance('Love is the best thing in this world. Some found their love and some are still looking for their love.');
-
 
 // const sumNumberFromStr = (str = "") => {
 //     console.log(str.match(/\d+/gi).map(str => Number(str)).reduce((a, b) => a + b));
@@ -125,23 +115,19 @@ for (let i = 1; i <= 5; i++){
 
 // sumNumberFromStr('He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.');
 
-
-
-
-
-// Day 3 
+// Day 3
 
 // Truthy value -> All string ascept "" and all array acept [], boolean true , all number acept 0
-// Falsy value -> false, null, [], "", 0, NaH, undefined 
+// Falsy value -> false, null, [], "", 0, NaH, undefined
 
 const falsyValue = () => {
-    console.log(!null);
-    console.log(!NaN);
-    console.log([]== false);
-    console.log(0== false);
-    console.log(!undefined)
-    console.log(false == false)
-}
+  console.log(!null);
+  console.log(!NaN);
+  console.log([] == false);
+  console.log(0 == false);
+  console.log(!undefined);
+  console.log(false == false);
+};
 
 // falsyValue()
 /*true
@@ -151,19 +137,16 @@ true
 true
 true */
 
-
-// logical operator 
+// logical operator
 const numerCompare = (a, b, c) => {
-    if (a > b && a > c){
-        return `${a} is biggest`
-    }
-    else if (b > a && b > c){
-        return `${b} is biggest`
-    }
-    else{
-        return `${c} is biggest`
-    }
-}
+  if (a > b && a > c) {
+    return `${a} is biggest`;
+  } else if (b > a && b > c) {
+    return `${b} is biggest`;
+  } else {
+    return `${c} is biggest`;
+  }
+};
 
 // console.log(numerCompare(100, 20, 25)) // 100 is biggest
 
@@ -171,34 +154,33 @@ const numerCompare = (a, b, c) => {
 // console.log(a++); //post increament //10
 // console.log(++a); // pre-increameb //12
 
-
-// Date Object 
-
+// Date Object
 
 const date = new Date();
-function dateGetting () {
-    console.log(date.toUTCString());
+function dateGetting() {
+  console.log(date.toUTCString());
 }
 
 // dateGetting();
 const startTime = () => {
-    setInterval(() => {
-        document.querySelector('.time').innerText = date.toLocaleTimeString();
-    }, 500); 
-}
+  setInterval(() => {
+    document.querySelector(".time").innerText = date.toLocaleTimeString();
+  }, 500);
+};
 
 // startTime();
 
 //There is no 'on' in both dragon and python
 
 const bothWordHaveText = (text = "", word = []) => {
-    if (word[0].includes(text) && word[word.length - 1].includes(text)){
-        console.log(`Both ${word[0]} and ${word[word.length - 1]} includes ${text}`);
-    }
-}
+  if (word[0].includes(text) && word[word.length - 1].includes(text)) {
+    console.log(
+      `Both ${word[0]} and ${word[word.length - 1]} includes ${text}`
+    );
+  }
+};
 
 // bothWordHaveText("on", ["dragon", "python"]);
-
 
 // Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
 /*
@@ -252,26 +234,25 @@ ageCalculateBtn.addEventListener("click", () => {
     }
 }) */
 
-
 // Day 04 conditions
 
 const readyToDrive = (age) => {
-    let message = "";
+  let message = "";
 
-    switch (true) {
-        case age >= 18:
-            message = "Youre ready to drive";
-            break;
-        case age < 18 :
-            message = `Your are ${18 - age} years younger to drive`;
-            break;
-        default:
-            message = "Dont know"
-            break;
-    }
+  switch (true) {
+    case age >= 18:
+      message = "Youre ready to drive";
+      break;
+    case age < 18:
+      message = `Your are ${18 - age} years younger to drive`;
+      break;
+    default:
+      message = "Dont know";
+      break;
+  }
 
-    console.log(message);
-}
+  console.log(message);
+};
 
 // readyToDrive(16);
 
@@ -288,19 +269,147 @@ const readyToDrive = (age) => {
 //   Enter a month: FEbruary
 //   February has 28 days.
 
-
 const howManyDays = (month = "") => {
-    const monthLower = month.toLowerCase();
-    let numOfdays = 30;
-    if (monthLower == "january" || monthLower == "march" || monthLower == "may" || monthLower == "july" || monthLower == "august" || monthLower == "october" || monthLower == "december"){
-        numOfdays = 31;
-    }
-    else if (monthLower == "february"){
-        numOfdays = 28;
+  const monthLower = month.toLowerCase();
+  let numOfdays = 30;
+  if (
+    monthLower == "january" ||
+    monthLower == "march" ||
+    monthLower == "may" ||
+    monthLower == "july" ||
+    monthLower == "august" ||
+    monthLower == "october" ||
+    monthLower == "december"
+  ) {
+    numOfdays = 31;
+  } else if (monthLower == "february") {
+    numOfdays = 28;
+  }
+
+  return `${month} have ${numOfdays} of days.`;
+};
+
+// const month = prompt("Enter the month: ");
+// console.log(howManyDays(month));
+
+// Arrays in js - Day 05
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
+
+// countries[countries.findIndex(country => country == "Japan")] = "India";
+// console.log(countries);
+
+// const arrayOfFourFname = new Array(4).fill("Manik");
+// // console.log(arrayOfFourFname);
+
+// const fruits = ["banana", "orange", "mango", "lemon"];
+// console.log(fruits.indexOf("orange")); // 1
+
+// const isIncludeInFruitArray = (fruit = "") => {
+//   const indexOfFruit = fruits.indexOf(fruit.toLowerCase());
+//   if (indexOfFruit !== -1) {
+//     return `${fruit} is available in array.`;
+//   }
+//   return `${fruit} isn't available in fruit array`;
+// };
+
+// // console.log(isIncludeInFruitArray("apple"));
+
+// // console.log(fruits.includes("bat")); // false
+
+
+// // console.log(Array.isArray(fruits)); // true 
+
+
+// // slice and splice 
+// console.log(fruits.slice(0, 3)); //copy
+// console.log(fruits); 
+
+// console.log(fruits.splice(0, 2));
+// console.log(fruits);
+
+// const numbers = [101, 33, 25, 5, 32];
+// console.log(numbers.sort((a, b) => {
+//     if (a > b) {
+//         return 1;
+//     }
+//     else{
+//         return -1;
+//     }
+// })); // sorting
+
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+
+  const todoTextboxEle = document.getElementById("todoInput");
+  const todoAddBtnEle = document.querySelector(".add-todo-btn");
+  const todoListContainerEle = document.querySelector(".todo-list");
+
+
+  const markTodo = (ele) => {
+    ele.style.textDecoration = "line-through";
+  }
+
+  const makeUniqueID = () => {
+    return `${(Math.floor(Math.random()*100000))}`;
+  }
+
+  const createHTML = (txt = "") => {
+    const html = document.createElement("p");
+    html.classList.add("todo-item");
+    html.setAttribute("id", makeUniqueID());
+    html.addEventListener("click", (e)=> {
+        markTodo(e.target);
+    })
+    html.addEventListener("dblclick", (e) => {
+        e.target.remove();
+    })
+    html.innerText = txt;
+    return html;
+  }
+
+
+  todoAddBtnEle.addEventListener('click', () => {
+    const inputText = todoTextboxEle.value;
+    if (inputText != ""){
+        todoTextboxEle.value = "";
+        let tempHTML = createHTML(inputText);
+        todoListContainerEle.insertAdjacentElement("beforeend", tempHTML);
     }
 
-    return `${month} have ${numOfdays} of days.`;
-}
-
-const month = prompt("Enter the month: ");
-console.log(howManyDays(month));
+    
+  })
